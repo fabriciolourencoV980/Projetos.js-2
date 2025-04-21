83a7c80994d3250d46d36a65f1c62caa03d6430c
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2025-04-21 19:28:09 -0300

    .

---
+++ 2_OPERADORES/js/script.js
@@ -0,0 +1,30 @@
+// 1 - Number 
+
+console.log( typeof 10);
+console.log( typeof 5.14); 
+console.log( typeof -127);
+
+// 2 - Operadores aritméticas 
+
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(5 * 4); 
+console.log(10 / 2); 
+
+console.log(5 + 4 * 2);
+
+// 3 - Special numbers
+
+console.log( typeof Infinity );
+console.log( typeof -Infinity);
+console.log(12 * "asd");
+console.log( typeof NaN); 
+
+// 4 - Booleanos
+
+console.log(5 == 5);
+console.log(5 == "5"); 
+console.log(5 > 4); 
+console.log(5 < 4); 
+console.log(5 === 5); 
+console.log(5 === "5");
