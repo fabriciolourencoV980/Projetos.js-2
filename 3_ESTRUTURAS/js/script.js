4a0ec132b90918ac452e0917951377f6d5f4ca66
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2025-04-26 17:57:36 -0300

    Update script.js

--- 3_ESTRUTURAS/js/script.js
+++ 3_ESTRUTURAS/js/script.js
@@ -1,31 +1,31 @@
 // // 1 - Prompt
 
-// const age = prompt("Digite sua Idade"); 
-// console.log(`Você tem ${age} anos`); 
+/* const age = prompt("Digite sua Idade"); 
+ console.log(`Você tem ${age} anos`); */
 
-// // 2 - Alert
+// 2 - Alert
 
-// alert("Testando");
+/* alert("Testando");
 
-// const z = 10; 
-// alert(`O seu número é ${z}`); 
+const z = 10; 
+alert(`O seu número é ${z}`); */
 
 // 3 - Object Math
 
-// console.log(Math.max(5, 2, 1, 10)); 
-// console.log(Math.floor(5.14)); //Arredonda para baixo
-// console.log(Math.ceil(5.14)); //Arredonda para cima
-// console.log(Math.rando()); //Gera números aleátorios 
+/* console.log(Math.max(5, 2, 1, 10)); 
+ console.log(Math.floor(5.14)); //Arredonda para baixo
+ console.log(Math.ceil(5.14)); //Arredonda para cima
+ console.log(Math.rando()); //Gera números aleátorios */
 
 // 4 - console
  
-// console.log("Teste!"); 
-// console.error("ERROR1!"); 
-// console.warn("AVISO"); 
+/* console.log("Teste!"); 
+ console.error("ERROR1!"); 
+ console.warn("AVISO"); */
 
 // 5 - If
 
-const m = 10;
+/* const m = 10;
 
 if(m > 5){
     console.log("M é maior que 5!!")
@@ -35,4 +35,25 @@ const user = "João";
 
 if(user === "João"){
     console.log("Olá João")
+}; */
+
+// 6 - Else
+
+const loggedIn = false; 
+
+if(loggedIn ===  true){
+    console.log("Está authenticado.")
+} else{
+    console.log("Não está authenticado.")
 }; 
+
+const q = 10; 
+const w = 25;
+
+if(q > 5 && w > 20){
+    console.log("Números mais altos.")
+} else{
+    console.log("Os números não são mais altos.")
+}
+
+// 7 - Else If
