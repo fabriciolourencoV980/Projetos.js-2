0924b3e82ba5789cd96b5ff69347795780e1bba6
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2025-04-26 17:30:10 -0300

    .

---
+++ 3_ESTRUTURAS/js/script.js
@@ -0,0 +1,28 @@
+// // 1 - Prompt
+
+// const age = prompt("Digite sua Idade"); 
+// console.log(`Você tem ${age} anos`); 
+
+// // 2 - Alert
+
+// alert("Testando");
+
+// const z = 10; 
+// alert(`O seu número é ${z}`); 
+
+// 3 - Object Math
+
+// console.log(Math.max(5, 2, 1, 10)); 
+// console.log(Math.floor(5.14)); //Arredonda para baixo
+// console.log(Math.ceil(5.14)); //Arredonda para cima
+// console.log(Math.rando()); //Gera números aleátorios 
+
+// 4 - console
+ 
+// console.log("Teste!"); 
+// console.error("ERROR1!"); 
+// console.warn("AVISO"); 
+
+// 5 - If
+
+
