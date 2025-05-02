d56312c0261ac3b5a71ef5d927a69e1ceb627e17
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2025-05-02 16:54:54 -0300

    Update 4_script.js

--- 0_TESTES/4_script.js
+++ 0_TESTES/4_script.js
@@ -7,11 +7,11 @@ function verificandoValor(x1, x2){
         return false
     } else if(x1 === x2){
         return true 
-    } else if (x1 == 'x2'){
+    } else if (x1 !== "x2"){
         return false
     }
 }; 
 
 console.log(verificandoValor(0, 0));
 console.log(verificandoValor(0, 1));
-console.log(verificandoValor(0, '1')); 
+console.log(verificandoValor(0, "0")); 
