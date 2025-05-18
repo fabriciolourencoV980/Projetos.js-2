9b8422a7af798f95286e5f5cabae90fe8ab826c1
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2025-05-18 18:42:43 -0300

    .

---
+++ 5_Arrays/js/script.js
@@ -0,0 +1,54 @@
+// 1 - Arrays
+/*const lista = [1, 2, 3, 4, 5]
+console.log(lista)
+console.log(typeof lista)
+
+// 2 - Mais sobre array 
+const arr = ["a", "b", "c", "d", "d"]
+console.log(arr[2])
+
+// 3 - propriedades
+const numbers = [5, 3, 4];
+console.log(numbers.length);
+console.log(numbers["length"]);
+
+const myName = "Fabricio"; 
+console.log(myName.length);
+
+// 4 - Métodos
+const otherNumbers = [1, 2, 3];
+const allNumbers = numbers.concat(otherNumbers);
+console.log(allNumbers);
+
+const text = "Algum Texto";
+const text1 = "ALGUM TEXTO"; 
+console.log(text.toUpperCase());
+console.log(text1.toLowerCase());
+console.log(typeof text.toLocaleUpperCase());
+console.log(text.indexOf("g")); 
+
+// 5 - Objetos
+const person = {
+    name: "Fabrício,",
+    age: 20 + " anos,", 
+    job: "Analista"
+}
+console.log(person); 
+console.log(person.name, person.age, person.job);
+console.log(person.name.length);*/
+
+// 6 - Criando e deletando Objetos
+const car = {
+  engine: 2.0,
+  brand: "VW",
+  model: "Tiguan",
+  km: 2000,
+};
+console.log(car);
+car.doors = 4;
+console.log(car);
+
+delete car.km;
+console.log(car);
+
+// 7 - 
