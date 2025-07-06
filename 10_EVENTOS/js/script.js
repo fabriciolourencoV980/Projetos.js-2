71e7da52d3eaf85c35a1b604d8e4e990ff5fc6d6
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2025-07-06 18:46:41 -0300

    .

--- 10_EVENTOS/js/script.js
+++ 10_EVENTOS/js/script.js
@@ -20,4 +20,72 @@ thirdBtn.addEventListener("click", () => {
 })
 
 
-// 3 - 
+// 3 - argumento do evento
+
+const myTitle = document.querySelector("#my-title")
+myTitle.addEventListener("click", (event)=>{
+    console.log(event);
+    console.log(event.offsetX);
+    console.log(event.pointerType);
+    console.log(event.target);
+})
+
+// 4 - Propagação
+const containerBtn = document.querySelector("#btn-container")
+const btnInsideContainer = document.querySelector("#div-btn")
+
+containerBtn.addEventListener("click", () => {
+    console.log("Evento 1"); 
+})
+
+btnInsideContainer.addEventListener("click", (e) => {
+    e.stopImmediatePropagation();
+    console.log("Evento 2"); 
+})
+
+// 5 - Ações Default / Removendo evento padrão
+const a = document.querySelector("a"); 
+a.addEventListener("click", (e) => {
+    e.preventDefault(); 
+    console.log("Não alterou a Página!")
+})
+
+// 6 - Eventos de teclado
+document.addEventListener("keyup", (e) => {
+    console.log(`Soltou a tecla ${e.key}`)
+})
+
+document.addEventListener("keydown", (e) => {
+    console.log(`Apertou a tecla ${e.key}`)
+})
+
+// 7 - Eventos do Mouse
+const mouseEvent = document.querySelector("#mouse") ; 
+mouseEvent.addEventListener("mousedown", ()=>{
+    console.log("Pressionou o Botão")
+})
+
+document.querySelector("#mouse")
+mouseEvent.addEventListener("mouseup", ()=>{
+    console.log("Soltou o Botão")
+})
+
+
+mouseEvent.addEventListener("dblclick", () => {
+    console.log("Clique Duplo")
+})
+
+// 8 - Movimento do mouse
+/*document.addEventListener("mousemove", (e) => {
+    console.log(`No eixo X ${e.x}`)
+    console.log(`No eixo Y ${e.y}`)
+})*/
+
+// 9 - Eventos por scroll
+window.addEventListener("scroll", (e) => {
+    if(window.pageYOffset > 200){
+        console.log("Passamos de 200 pixels")
+    }
+})
+
+// 10 - Evento de foco
