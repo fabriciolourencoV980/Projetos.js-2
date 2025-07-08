466320a434a1600a4f28e834de0537e005252fff
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2025-07-08 09:11:47 -0300

    Update script.js

--- 11_CALCULADORA/js/script.js
+++ 11_CALCULADORA/js/script.js
@@ -3,10 +3,30 @@ const multiplicationForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number"); 
 const multiplicationInput = document.querySelector("#multiplicator"); 
 
+const multiplicationTable = document.querySelector("#multiplication-operations"); 
 
+const multiplicationTitle = document.querySelector("#multiplication-title span");
 
 // Funções
+const createTable = (number, multiplicatiorNumber) => {
+    
+    multiplicationTable.innerHTML = "";
 
+        for(i = 1; i <= multiplicatiorNumber; i++){
+            const result = number * i; 
+            
+            const template = `<div class="row">
+                <div class="operation">${number} x ${i} =</div>
+                <div class="result">${result}</div>
+                </div>`
+
+                const parser = new DOMParser()
+                const htmlTemplate = parser.parseFromString(template, "text/html");
+                const row = htmlTemplate.querySelector(".row");
+                multiplicationTable.appendChild(row);
+        }
+        multiplicationTitle.innerText = number; 
+};
 
 
 
@@ -19,5 +39,5 @@ multiplicationForm.addEventListener("submit", (e) => {
 
     if(!multiplicationNumber || !multiplicatiorNumber) return;
 
-    console.log(multiplicationNumber, multiplicatiorNumber)
+    createTable(multiplicationNumber, multiplicatiorNumber);
 }); 
