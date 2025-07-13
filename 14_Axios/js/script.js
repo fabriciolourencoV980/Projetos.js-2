10f0e74519fd0fc82e99524fe887b0089c693236
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2025-07-12 22:45:55 -0300

    .

---
+++ 14_Axios/js/script.js
@@ -0,0 +1,36 @@
+// 1 - Instalação
+// console.log(axios);
+
+// 2 - Primeiro Request
+const getData = async () => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+
+    console.log(response);
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getData();
+
+// 3 - Imprimindo dados na tela
+const container = document.querySelector("#user-container");
+const printData = async () => {
+  const data = await getData();
+
+  console.log(data);
+  data.forEach((user) => {
+    const div = document.createElement("div");
+    const nameElement = document.createElement("h2")
+    nameElement.textContent = user.name
+    div.appendChild(nameElement)
+    container.appendChild(div)
+  });
+};
+
+printData();
